1ba130287c0387c054abb40fca2bda6373230c58
Justin Bax <justin.bax@icloud.com>
2021-06-10 19:24:30 -0400

    basic GLFW for Windows

---
+++ src/main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+#include "GLFW/glfw3.h"
+
+int main() {
+	printf("NESRev v3.6\n");
+
+	if (!glfwInit())
+		return 1;
+	printf("GLFW initialized.\n");
+
+	GLFWwindow *window = glfwCreateWindow(1920, 1090, "NESRev v3.6", NULL, NULL);
+	if (window == NULL)
+		return 1;
+	
+	glfwMakeContextCurrent(window);
+
+	while (!glfwWindowShouldClose(window)) {
+		glClear(GL_COLOR_BUFFER_BIT);
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+
+	glfwTerminate();
+
+	return 0;
+}
