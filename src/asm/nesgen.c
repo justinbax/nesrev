465779d94bc9c2eaf9de7475219e8745469ceffd
Justin Bax <justin.bax@icloud.com>
2021-11-28 16:01:36 -0500

    created .nes generator and sample assembly program

---
+++ src/asm/nesgen.c
@@ -0,0 +1,120 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+int ctoi(const char *value) {
+	int result = 0;
+	int multiplier = 1;
+	for (char *c = value; *c != '\0'; c++) {
+		result += (*c - '0') * multiplier;
+		multiplier *= 10;
+	}
+	return result;
+}
+
+uint8_t *readFile(const char *path, int *size) {
+	*size = 0;
+
+	FILE *file = fopen(path, "rb");
+	if (file == NULL) return NULL;
+
+	fseek(file, 0, SEEK_END);
+	*size = ftell(file);
+	rewind(file);
+
+	uint8_t *contents = malloc(*size * sizeof(uint8_t));
+	if (contents == NULL) {
+		*size = 0;
+		fclose(file);
+		return NULL;
+	}
+
+	if (fread(contents, sizeof(uint8_t), *size, file) != *size) {
+		*size = 0;
+		fclose(file);
+		free(contents);
+		return NULL;
+	}
+
+	fclose(file);
+	return contents;
+}
+
+bool emptyFill(FILE *output, int fillSize) {
+	if (fillSize != 0) {
+		uint8_t *padding = calloc(fillSize, sizeof(uint8_t));
+		if (padding == NULL)
+			return false;
+		if (fwrite(padding, sizeof(uint8_t), fillSize, output) != fillSize)
+			return false;
+	}
+	return true;
+}
+
+int main(int argc, const char *argv[]) {
+
+	if (argc != 6) {
+		printf("Usage : nesgen chr prg mapper mirror out\n");
+		return -0x01;
+	}
+
+	int chrSize, prgSize;
+	uint8_t *chr = readFile(argv[1], &chrSize);
+	uint8_t *prg = readFile(argv[2], &prgSize);
+	if (!chr || !prg) {
+		printf("Fatal error : couldn't read CHR or PRG file / out of memory.\n");
+		return -0x02;
+	}
+
+	FILE *output = fopen(argv[5], "w");
+	if (output == NULL) {
+		printf("Fatal error : couldn't open or create output file.\n");
+		free(chr);
+		free(prg);
+		return -0x03;
+	}
+
+	uint8_t header[16] = {
+		0x4E, 0x45, 0x53, 0x1A, // Constant NES + MSDOS EOF
+		0x00, 0x00, // PRG & CHR size
+		0x00, // Mostly mapper & mirroring
+		0x00, // For us, just upper nybble of mapper
+		0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 // Useless stuff
+	};
+
+	uint8_t mapper = ctoi(argv[3]);
+	header[4] = ((prgSize - 1) >> 14) + 1; // In 16KiB, or 2^14
+	header[5] = ((chrSize - 1) >> 13) + 1; // In 8KiB, or 2^13
+	header[6] |= (mapper & 0b1111) << 4;
+	header[6] |= (argv[4][0] == '1') || (argv[4][0] == 'v') || (argv[4][0] == 'V'); // '1', 'v' or 'V' for vertical mirroring
+
+	if ((header[6] & 0b1) == 0 && (argv[4][0] != '0') && (argv[4][0] != 'h') && (argv[4][0] != 'H')) {
+		// Not '1', 'v', 'V', '0', 'h' or 'H'
+		printf("Invalid mirroring type, assuming vertical.\n");
+		printf("Possible types : [H]orizontal, [V]ertical.\n");
+	}
+
+	header[7] |= mapper & 0b11110000;
+
+	uint8_t status = 0;
+
+	status |= (fwrite(header, sizeof(uint8_t), 16, output) != 16);
+	status |= (fwrite(prg, sizeof(uint8_t), prgSize, output) != prgSize);
+	free(prg);
+	status |= !emptyFill(output, (header[4] << 14) - prgSize);
+	status |= fwrite(chr, sizeof(uint8_t), chrSize, output);
+	free(chr);
+	status |= !emptyFill(output, (header[4] << 13) - chrSize);
+
+	if (status) {
+		freopen(NULL, "w", output);
+		fclose(output);
+		printf("Fatal error : couldn't write to output file.\n");
+		return -0x04;
+	}
+
+	fclose(output);
+
+	return 0;
+}
---
+++ src/asm/sample.asm
@@ -0,0 +1,165 @@
+PPUCTRL = $2000
+PPUMASK = $2001
+PPUSTATUS = $2002
+PPUSCROLL = $2005
+PPUADDR = $2006
+PPUDATA = $2007
+OAMDMA = $4014
+JOY1 = $4016
+
+ControllerMirror = $10
+InputA = ControllerMirror | $07
+InputB = ControllerMirror | $06
+InputSelect = ControllerMirror | $05
+InputStart = ControllerMirror | $04
+InputUp = ControllerMirror | $03
+InputDown = ControllerMirror | $02
+InputLeft = ControllerMirror | $01
+InputRight = ControllerMirror | $00
+
+OAMMirror = $02
+
+	.org $C000
+
+Reset:
+	sei
+	cld
+	lda #$FF
+	tax
+	inx
+	stx PPUCTRL
+	stx PPUMASK
+
+	bit PPUSTATUS ; Reset VBlank flag (bit 7)
+VBlank1:
+	lda PPUSTATUS
+	bpl VBlank1
+
+	; RAM Reset
+	jsr InitRAMPtr
+	
+ClearRAM:
+	dey
+	sta ($00),Y
+	bne ClearRAM
+	dec $01
+	bpl ClearRAM
+
+VBlank2:
+	lda PPUSTATUS
+	bpl VBlank2
+
+	; PPU is warmed up
+	; Fill nametables (in VRAM) with tile 0
+	lda #$20
+	sta PPUADDR
+	jsr InitRAMPtr
+	sta PPUADDR
+ClearVRAM:
+	dey
+	sta PPUDATA
+	bne ClearVRAM
+	dec $01
+	bpl ClearVRAM
+
+	; Palette initialization
+	lda #$3F
+	sta PPUADDR
+	lda #$00
+	sta PPUADDR
+	ldx #$00
+WritePalette:
+	lda Palette,X
+	sta PPUDATA
+	inx
+	cpx #$20
+	bne WritePalette
+
+	; Sprite 0 initialization
+	lda #$01
+	sta (OAMMirror << 8) | $01 ; Pattern $01
+
+
+	; Ready to render
+	lda #%10010000
+	sta PPUCTRL
+	lda #%00011110
+	sta PPUMASK
+
+InfiniteLoop:
+	jmp InfiniteLoop
+
+InitRAMPtr:
+	ldy #$07
+	sty $01 ; High byte of RAM address
+	ldy #$00 ; Low byte
+	sty $00
+	tya
+	rts
+
+NMI:
+	lda #OAMMirror ; DMA
+	sta OAMDMA
+
+	; Controller reading
+	ldx #%00000001
+	stx JOY1
+	dex
+	stx JOY1
+	ldx #$08
+ReadInput:
+	; Weird but efficient way to fit controller status in one byte
+	; The idea behind this is to avoid long RMW instructions, so we instead handle logic (ANDs, ASLs, ORAs) directly with A, using the carry flag as medium between "past A and future A" (before and after PLA)
+	; pha
+	; lda JOY1
+	; lsr ; Button status is now carry flag
+	; pla
+	; rol ; Shifts left, bit 0 is now current button's status
+	; bne ReadInput
+	; sta ControllerMirror
+
+	; We don't actually need the extra memory so it's best to just use 8 bytes
+	lda JOY1
+	and #%00000001
+	dex
+	sta ControllerMirror,X
+	bne ReadInput
+
+	; Sprite 0 position update based on controller status
+	lda InputUp
+	cmp #$01
+	bne ReadDown
+	dec (OAMMirror << 8) | $00
+ReadDown:
+	lda InputDown
+	cmp #$01
+	bne ReadLeft
+	inc (OAMMirror << 8) | $00
+ReadLeft:
+	lda InputLeft
+	cmp #$01
+	bne ReadRight
+	dec (OAMMirror << 8) | $03
+ReadRight:
+	lda InputRight
+	cmp #$01
+	bne EndInputRead
+	inc (OAMMirror << 8) | $03
+
+EndInputRead:
+	rti
+
+Palette:
+	.db $00,$16,$2A,$02 ; BG0 : default + RGB
+	.db $00,$00,$00,$00
+	.db $00,$00,$00,$00
+	.db $00,$00,$00,$00
+	.db $00,$16,$2A,$02 ; SP0 : default + RGB
+	.db $00,$00,$00,$00
+	.db $00,$00,$00,$00
+	.db $00,$00,$00,$00
+
+	.org $FFFA
+	.dw NMI
+	.dw Reset
+	.dw Reset ; IRQ unused
